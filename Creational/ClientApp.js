7edce7a6f9c657ee3eff61462e25c1c43b035684
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-09-01 18:21:40 -0500

    challenge singleton parttener

--- Creational/ClientApp.js
+++ Creational/ClientApp.js
@@ -1,72 +1,125 @@
 class Product{
-  constructor(name, id, cost){
-    this.name = name;
-    this.id = id;
-    this.cost = cost;
+  constructor(id, name, cost){
+    this._name = name;
+    this._id = id;
+    this._cost = cost;
   }
 
-  getName(){
-    return this.name;
+  get name(){
+    return this._name;
   }
 
-  setName(name){
-    this.name = name;
+  set name(name){
+    this._name = name;
   }
 
-  getId(){
-    return this.id;
+  get id(){
+    return this._id;
   }
 
-  getCost(){
-    return this.cost;
+  set id(id){
+    this._id = id;
+  }
+
+  get cost(){
+    return this._cost;
+  }
+
+  set cost(cost){
+    this._cost = cost;
   }
 }
 
 class ShoppingCar{
-  static instance = undefined;
+  static _instance = undefined;
 
   constructor(){
-    this.products = [];
-  }
-
-  getProducts(){
-    return this.products;
-  }
-
-  add(product){
-    this.products.push(product);
-  }
-
-  deleteById(id){
-    this.products = this.products.filter( product => product.id !== id);
+    this._products = [];
   }
 
   static getInstance(){
-    if (!ShoppingCar.instance) {
-      ShoppingCar.instance = new ShoppingCar();
+    if (!ShoppingCar._instance) {
+      ShoppingCar._instance = new ShoppingCar();
     }
-    return ShoppingCar.instance;
+    return ShoppingCar._instance;
+  }
+
+  get products(){
+    return this._products;
+  }
+  
+
+  add(product){
+    this._products.push(product);
+  }
+
+  deleteById(id) {
+    this._products = this._products.filter( product => product.id !== id);
   }
 }
 
-function ClientApp(){
-  const car = ShoppingCar.getInstance();
-  const glasses = new Product("glasses", 112, 20);
-  const shoes = new Product('shoes', 2166, 250)
-  console.log(glasses.getName(), glasses.getCost(), glasses.getId());
-  // adding products to the ShoppingCar. 
-  console.log(car);
-  car.add(glasses);
-  console.log('adding glasses to the car', car.getProducts());
+function appSingleton() {
+  // Create new shopping car
+  const shoppingCar = ShoppingCar.getInstance();
 
-  // checking that the car is by myself
-  const car2 = ShoppingCar.getInstance();
-  car2.add(shoes);
-  console.log('testing that car has products', car2.getProducts());
+  // First product
+  shoppingCar.add(
+    new Product(
+      'BK001',
+      'Design Patterns: Elements of Reusable Object-Oriented Software',
+      750
+    )
+  );
 
-  // delete product both cars. 
-  car.deleteById(112);
-  console.log('delete glasses of the cart', car.getProducts(), car2.getProducts());
+  // Second product
+  shoppingCar.add(
+    new Product('BK002', 'Introduction to Algorithms', 1000)
+  );
+
+  // Get existing shopping car instance
+  const shoppingCarNewInstance = ShoppingCar.getInstance();
+  shoppingCarNewInstance.add(new Product('BK003', 'Compilers', 900));
+
+  console.log('\n--- Shopping Car products ---\n');
+  console.log(shoppingCar.products);
+
+  console.log('\n--- Shopping Car New Instance products ---\n');
+  console.log(shoppingCarNewInstance.products);
+
+  // Products list must be the same
+  console.log('\n--- Are shopping cars products the same? ---\n');
+  console.log(shoppingCar.products === shoppingCarNewInstance.products); // true
+
+  // The number of elements in the list must be the same, in this case 3
+  console.log(
+    '\n--- Is shopping car number of products in both instances equal? ---\n'
+  );
+  console.log(
+    shoppingCar.products.length ===
+      shoppingCarNewInstance.products.length
+  );
+
+  // Let's delete second product
+  shoppingCarNewInstance.deleteById('BK002');
+  console.log('\n--- Product deleted: BK002---\n');
+
+  console.log('\n--- Shopping Car products ---\n');
+  console.log(shoppingCar.products);
+
+  console.log('\n--- Shopping Car New Instance products ---\n');
+  console.log(shoppingCarNewInstance.products);
+
+  // The number of elements in the list must be the same, in this case 2
+  console.log(
+    '\n--- Is shopping car number of products in both instances equal? ---\n'
+  );
+
+  console.log(
+    shoppingCar.products.length)
+  console.log(
+    shoppingCar.products.length ===
+      shoppingCarNewInstance.products.length
+  );
 }
 
-ClientApp();
+appSingleton();
